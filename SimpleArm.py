452d65bad7c2c844a7d8b09a3b60a0602caa8048
Oliver-Molina <98718412+Oliver-Molina@users.noreply.github.com>
2024-11-01 12:17:47 -0600

    3D Arm with 3DOF

--- SimpleArm.py
+++ SimpleArm.py
@@ -8,29 +8,36 @@ l2 = 1
 
 x = 0.7
 y = 0.7
+z = 0.5
 
-frames = 500
+frames = 5000
 interval = 50
 
+save_animation = False
+
 fig = plt.figure()
-ax = fig.add_subplot()
+ax = fig.add_subplot(projection='3d')
 lines = ax.plot([0, l1], [0, 0], [0, 0], [0, l2], marker='o')
 
-def generate_arm_angles(x, y, l1, l2):
+
+def magnitude(x):
+    return math.sqrt(sum(i**2 for i in x))
+
+def get_colour(i):
+    colours = ['b', 'g', 'r', 'c', 'm', 'y']
+    return colours[i % len(colours)]
+
+def generate2JointSinglePlainArm(x, y, l1, l2):
     theta2 = math.acos((x**2 + y**2 - l2**2 - l1**2)/(2*l2*l1))
     theta1 = math.atan(y/x) - math.atan((l2*math.sin(theta2))/(l2*math.cos(theta2) + l1))
     return [theta1, theta2]
 
-
-def magnitude(a, b):
-    return math.sqrt(a**2 + b**2)
-
-def update2dArm(frame):
+def update2JointSinglePlainArm(frame):
     # Plot Data
     desiredx = x + 0.5 * math.cos(2*math.pi*frame/interval)
     desiredy = y + 0.5 * math.sin(2*math.pi*frame/interval)
 
-    theta1, theta2 = generate_arm_angles(desiredx, desiredy, l1 , l2)
+    theta1, theta2 = generate2JointSinglePlainArm(desiredx, desiredy, l1 , l2)
 
     x1 = l1*math.cos(theta1)
     y1 = l1*math.sin(theta1)
@@ -41,13 +48,58 @@ def update2dArm(frame):
     for artist in plt.gca().lines + plt.gca().collections:
         artist.remove()
 
-    ax.plot([0, x1], [0, y1], [x1, x2], [y1, y2], color='b', marker='o')
+    ax.plot([0, x1], [0, y1], [0,0], color='b', marker='o')
+    ax.plot([x1, x2], [y1, y2], [0,0], color='r', marker='o')
+
+def generate3Joint3DArmAngles(x, y, z, l1, l2):
+    theta3 = math.acos((x**2 + y**2 + z**2 - l2**2 - l1**2)/(2*l2*l1))
+    theta2 = math.atan(z/math.sqrt(x**2 + y**2)) - math.atan((l2*math.sin(theta3))/(l2*math.cos(theta3) + l1))
+    theta1 = math.atan(y/x)
+    return [theta1, theta2, theta3]
+
+
+def update3Joint3DArm(frame):
+    # Animate Initial Values
+    desiredx = x + 0.5 * math.cos(2*math.pi*frame/interval)
+    desiredy = y + 0.5 * math.sin(2*math.pi*frame/interval)
+    desiredz = z + 0.5 * math.sin(2*math.pi*frame/interval)
+
+    # Generate Angles
+    theta1, theta2, theta3 = generate3Joint3DArmAngles(desiredx, desiredy, desiredz, l1 , l2)
+
+    # Compute all positions using forward Kinematics
+
+    P0 = np.array([0, 0, 0])
+
+    r1 = l1*math.cos(theta2)
+
+    P1 = np.add(P0, [r1*math.cos(theta1), r1*math.sin(theta1), l1*math.sin(theta2)])
+
+    r2 = l2*math.cos(theta2 + theta3)
+
+    P2 = np.add(P1, [r2*math.cos(theta1), r2*math.sin(theta1), l2*math.sin(theta2 + theta3)])
+
+    # Wipe old plot
+    for artist in plt.gca().lines + plt.gca().collections:
+        artist.remove()
+
+    # Plot lines connecting each point (origin, P1), (P1, P2)
+    # ax.plot([0, x1], [0, y1], [0,0], color='b', marker='o')
+    # ax.plot([x1, x2], [y1, y2], [0,0], color='r', marker='o')
+
+    P = [P0, P1, P2]
+    if len(P) < 2:
+        return
+    
+    for i in range(1, len(P)):
+        ax.plot([P[i - 1][0], P[i][0]], [P[i - 1][1], P[i][1]], [P[i - 1][2], P[i][2]], color=get_colour(i))
+
 
 def main():
     maxmag = l1 + l2
-    ticks_frequency = 0.25
+    ticks_frequency = 0.5
 
-    if (magnitude(x,y) > maxmag):
+    if (magnitude([x,y]) > maxmag):
         print("Position impossible!")
         return False
 
@@ -63,14 +115,15 @@ def main():
     ax.spines['right'].set_visible(False)
 
     # Create 'x' and 'y' labels placed at the end of the axes
-    ax.set_xlabel('x', size=14, labelpad=-24, x=1.03)
-    ax.set_ylabel('y', size=14, labelpad=-21, y=1.02, rotation=0)
+    ax.set_xlabel('x', size=14, labelpad=0, x=1.2*maxmag)
+    ax.set_ylabel('y', size=14, labelpad=0, y=1.2*maxmag, rotation=0)
+    ax.set_zlabel('z', size=14, labelpad=0, y=1.2*maxmag, rotation=0)
 
     # Create custom major ticks to determine position of tick labels
-    x_ticks = np.arange(-maxmag, maxmag+1, ticks_frequency)
-    y_ticks = np.arange(-maxmag, maxmag+1, ticks_frequency)
-    ax.set_xticks(x_ticks[x_ticks != 0])
-    ax.set_yticks(y_ticks[y_ticks != 0])
+    ticks = np.arange(-maxmag, maxmag+1, ticks_frequency)
+    ax.set_xticks(ticks)
+    ax.set_yticks(ticks)
+    ax.set_zticks(ticks)
 
     # Create minor ticks placed at each integer to enable drawing of minor grid
     # lines: note that this has no effect in this example with ticks_frequency=1
@@ -85,15 +138,19 @@ def main():
     ax.plot((1), (0), marker='>', transform=ax.get_yaxis_transform(), **arrow_fmt)
     ax.plot((0), (1), marker='^', transform=ax.get_xaxis_transform(), **arrow_fmt)
 
-    # ax.set_xlim(-maxmag, maxmag)
-    # ax.set_ylim(-maxmag, maxmag)
-    # ax.set_zlim(-maxmag, maxmag)
+    ax.set_xlim(-maxmag, maxmag)
+    ax.set_ylim(-maxmag, maxmag)
+    ax.set_zlim(-maxmag, maxmag)
 
-    plt.axis([-maxmag, maxmag,-maxmag, maxmag])
 
-    ani = animation.FuncAnimation(fig=fig, func=update2dArm, frames=frames, interval=interval, repeat=False)
+    ani = animation.FuncAnimation(fig=fig, func=update3Joint3DArm, frames=frames, interval=interval, repeat=False)
 
-    plt.show()
+    if save_animation:
+        filepath = "animation.gif" 
+        writergif = animation.PillowWriter(fps=1/(interval/1000)) 
+        ani.save(filepath, writer=writergif)
+    else:
+        plt.show()
 
 
 
